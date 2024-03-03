b73dda5414e58cd7d08dc34ab24dfc6436408453
Romir Patel <romirpatel308@gmail.com>
2024-03-03 07:52:54 -0500

    Final Commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
+import { Home } from "./screens/Home";
+import { Rewards } from './screens/Home/Rewards';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
+
+<Rewards/>
+    
   </React.StrictMode>
 );
 
